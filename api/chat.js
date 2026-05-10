26585da03bb3e804339b988b7b539fce26b397df
Ben Jacobs <benjaminmjacobs@gmail.com>
2026-05-10 17:14:20 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,7 @@ const { GoogleAuth } = require("google-auth-library");
 
 const PROJECT_ID = "346318948573";
 const LOCATION = "us-west1";
-const REASONING_ENGINE_ID = "4207215272987197440";
+const REASONING_ENGINE_ID = "6841821054998937600";
 
 const BASE_URL =
   `https://${LOCATION}-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/reasoningEngines/${REASONING_ENGINE_ID}`;
