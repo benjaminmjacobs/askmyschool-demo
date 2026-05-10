dba51113af889d29d899dd41170153174ac6934c
Ben Jacobs <benjaminmjacobs@gmail.com>
2026-05-09 21:13:04 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -16,7 +16,7 @@ module.exports = async function handler(req, res) {
     const client = await auth.getClient();
 
     const endpoint =
-      "https://us-west1-aiplatform.googleapis.com/v1/projects/346318948573/locations/us-west1/reasoningEngines/5673699901649715200:query";
+     "https://us-west1-aiplatform.googleapis.com/v1/projects/pinevera-askmyschool/locations/us-west1/reasoningEngines/5673699901649715200:query";
 
     const response = await client.request({
       url: endpoint,
