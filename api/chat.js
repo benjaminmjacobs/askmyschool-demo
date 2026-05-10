65e95b97c7adfec787ab782db9ce3d6fa9f0cd7b
Ben Jacobs <benjaminmjacobs@gmail.com>
2026-05-10 14:14:06 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,16 @@
 const { GoogleAuth } = require("google-auth-library");
 
+function getCurrentDateForAgent() {
+  const formatter = new Intl.DateTimeFormat("en-CA", {
+    timeZone: "America/New_York",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  return `${formatter.format(new Date())}[America/New_York]`;
+}
+
 module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
@@ -17,21 +28,19 @@ module.exports = async function handler(req, res) {
       });
     }
 
-    const credentials = JSON.parse(
-      process.env.GOOGLE_SERVICE_ACCOUNT_JSON
-    );
+    const currentDate = getCurrentDateForAgent();
+
+    const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
 
     const auth = new GoogleAuth({
       credentials,
-      scopes: [
-        "https://www.googleapis.com/auth/cloud-platform",
-      ],
+      scopes: ["https://www.googleapis.com/auth/cloud-platform"],
     });
 
     const client = await auth.getClient();
 
     const endpoint =
-      "https://us-west1-aiplatform.googleapis.com/v1/projects/346318948573/locations/us-west1/reasoningEngines/3007568922246381568:query";
+      "https://us-west1-aiplatform.googleapis.com/v1/projects/346318948573/locations/us-west1/reasoningEngines/6128000514060713984:streamQuery";
 
     const response = await client.request({
       url: endpoint,
@@ -41,12 +50,14 @@ module.exports = async function handler(req, res) {
           user_id: "demo-user",
           session_id: "demo-session",
           message: message,
+          state: {
+            current_date: currentDate,
+          },
         },
       },
     });
 
     return res.status(200).json(response.data);
-
   } catch (error) {
     console.error("AskMySchool API error:", error);
 
